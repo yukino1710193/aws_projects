020aaf341dda9152b94a93d65695a117b4e5dd11
yukino1710193 <sasukevp1959@gmail.com>
2024-10-14 20:14:25 +0700

    hello

--- aws_scripts/auto_create_vpc.sh
+++ aws_scripts/auto_create_vpc.sh
@@ -28,31 +28,41 @@ do
     NATGW_ID=$(aws ec2 create-nat-gateway --subnet-id $SUBNET_ID --allocation-id $EIP_NAT_ID --region $REGION --query "NatGateway.NatGatewayId" --output text) && aws ec2 create-tags --resources $NATGW_ID --tags Key=Name,Value=MyNatGateway --region $REGION
 
     ## Create a custom route table
+# Public   
     ROUTE_TABLE_PUBLIC_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID --region $REGION_ID --query "RouteTable.RouteTableId" --output text) && aws ec2 create-tags --resources $ROUTE_TABLE_PUBLIC_ID --tags Key=Name,Value=$ROUTE_TABLE_PUBLIC_NAME --region $REGION_ID
+# Private    
     ROUTE_TABLE_PRIVATE_ID=$(aws ec2 create-route-table --vpc-id $VPC_ID --region $REGION_ID --query "RouteTable.RouteTableId" --output text) && aws ec2 create-tags --resources $ROUTE_TABLE_PRIVATE_ID --tags Key=Name,Value=$ROUTE_TABLE_PRIVATE_NAME --region $REGION_ID
     
     # Config route-tables
 # Public
-    aws ec2 create-route --route-table-id $ROUTE_TABLE_PUBLIC_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID
-## Private
-    aws ec2 create-route --route-table-id $ROUTE_TABLE_PRIVATE_NAME --destination-cidr-block 0.0.0.0/0 --nat-gateway-id $NATGW_ID
+    aws ec2 create-route --route-table-id $ROUTE_TABLE_PUBLIC_ID --destination-cidr-block 0.0.0.0/0 --gateway-id $IGW_ID --region $REGION
+# Private
+    aws ec2 create-route --route-table-id $ROUTE_TABLE_PRIVATE_NAME --destination-cidr-block 0.0.0.0/0 --nat-gateway-id $NATGW_ID --region $REGION
     
     # Associate subnet with custom route table to make public
-    aws ec2 associate-route-table  --subnet-id $SUBNET_PUB_ID --route-table-id $ROUTE_TABLE_PUBLIC_ID
-    aws ec2 associate-route-table  --subnet-id $SUBNET_PRI_ID --route-table-id $ROUTE_TABLE_PRIVATE_ID
+# Public
+    aws ec2 associate-route-table  --region $REGION --subnet-id $SUBNET_PUB_ID --route-table-id $ROUTE_TABLE_PUBLIC_ID
+# Private
+    aws ec2 associate-route-table  --region $REGION --subnet-id $SUBNET_PRI_ID --route-table-id $ROUTE_TABLE_PRIVATE_ID
     ## Configure subnet to issue a public IP to EC2 instances
-    aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PUB_ID --map-public-ip-on-launch
-    aws ec2 modify-subnet-attribute --subnet-id $SUBNET_PRI_ID --no-map-public-ip-on-launch
-done
+# Public
+    aws ec2 modify-subnet-attribute --region $REGION --subnet-id $SUBNET_PUB_ID --map-public-ip-on-launch
+# Private
+    aws ec2 modify-subnet-attribute --region $REGION --subnet-id $SUBNET_PRI_ID --no-map-public-ip-on-launch
+
 ##### CLEAN UP #####
 
 ## Run commands in the following order replacing all values as necessary
 
 # aws ec2 terminate-instances --instance-ids i-0c7371bbf7888bcdb
-# aws ec2 delete-security-group --group-id sg-01f034bccc4510b66
-# aws ec2 delete-subnet --subnet-id subnet-02a37f7b7ee80cc01
-# aws ec2 delete-subnet --subnet-id subnet-00075b1fb13da041d
-# aws ec2 delete-route-table --route-table-id rtb-073ec521b6d5ceff7
-# aws ec2 detach-internet-gateway --internet-gateway-id igw-0aa19bd4246f502d8 --vpc-id vpc-08c24bb183d741db9
-# aws ec2 delete-internet-gateway --internet-gateway-id igw-0aa19bd4246f502d8
-# aws ec2 delete-vpc --vpc-id vpc-08c24bb183d741db9
+aws ec2 delete-security-group --group-id $GROUP_ID --region $REGION
+aws ec2 delete-subnet --subnet-id $SUBNET_PRI_ID --region $REGION
+aws ec2 delete-subnet --subnet-id $SUBNET_PUB_ID --region $REGION
+aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_PRIVATE_ID --region $REGION
+aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_PUBLIC_ID --region $REGION
+aws ec2 detach-internet-gateway --internet-gateway-id $IGW_ID --vpc-id $VPC_ID --region $REGION
+aws ec2 delete-internet-gateway --internet-gateway-id $IGW_ID --region $REGION
+aws ec2 delete-nat-gateway --nat-gateway-id $NATGW_ID --region $REGION
+aws ec2 release-address --allocation-id $EIP_NAT_ID --region $REGION
+aws ec2 delete-vpc --vpc-id $VPC_ID --region $REGION
+done
