5b98ec7601129822e3ee216149f1c45cd5a6c8a3
yukino1710193 <sasukevp1959@gmail.com>
2024-10-14 20:22:27 +0700

    cicd

--- aws_scripts/auto_create_vpc_vpc_demo.sh
+++ aws_scripts/auto_create_vpc_vpc_demo.sh
@@ -96,3 +96,11 @@ aws ec2 delete-route-table --route-table-id rtb-073ec521b6d5ceff7
 aws ec2 detach-internet-gateway --internet-gateway-id igw-0aa19bd4246f502d8 --vpc-id vpc-08c24bb183d741db9
 aws ec2 delete-internet-gateway --internet-gateway-id igw-0aa19bd4246f502d8
 aws ec2 delete-vpc --vpc-id vpc-08c24bb183d741db9
+
+
+
+PCX_ID=$(aws ec2 create-vpc-peering-connection --vpc-id vpc-12345678 --peer-vpc-id vpc-87654321 --region us-east-1 --peer-region us-west-2 --query "VpcPeeringConnection.VpcPeeringConnectionId" --output text)
+aws ec2 create-vpc-peering-connection --vpc-id vpc-xxxxxxxx --peer-vpc-id vpc-yyyyyyyy --region <region1> --peer-region <region2>
+aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id pcx-xxxxxxxx --region <region2>
+aws ec2 create-route --route-table-id rtb-xxxxxxxx --destination-cidr-block <CIDR của VPC peer> --vpc-peering-connection-id pcx-xxxxxxxx --region <region1>
+aws ec2 create-route --route-table-id rtb-yyyyyyyy --destination-cidr-block <CIDR của VPC đầu tiên> --vpc-peering-connection-id pcx-xxxxxxxx --region <region2>
